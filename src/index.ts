d69bc42c7bf23b0d5c535ae90858f03bc17a8969
tris203 <tris203@gmail.com>
2023-09-29 12:24:40 +0100

    Revert "bug: hot fix for updating files, sizes and renames should now display…"

--- src/index.ts
+++ src/index.ts
@@ -163,7 +163,6 @@ class DiscordFileSystem extends FileSystem {
         // );
       });
     });
-    await this.fileManager.getFile(`${this.realcwd}/${fileName}`);
     return Promise.resolve(stream);
   }
   delete(fileName: string) {
